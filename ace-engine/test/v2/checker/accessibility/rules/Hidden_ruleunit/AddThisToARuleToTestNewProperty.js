de4093046d49fa70aac3bdf1d32294b619e78ce3
panbed <contact@bedp.dev>
2025-09-29 10:25:58 -0400

    temp

--- ace-engine/test/v2/checker/accessibility/rules/Hidden_ruleunit/AddThisToARuleToTestNewProperty.js
+++
@@ -1,6 +0,0 @@
-        Firebug.Console.log("ruleContext.ownerDocument.PT_CHECK_HIDDEN_CONTENT");
-        Firebug.Console.log(ruleContext.ownerDocument.PT_CHECK_HIDDEN_CONTENT);
-
-        Firebug.Console.log("ruleContext.PT_NODE_HIDDEN");
-        Firebug.Console.log(ruleContext.aceCache && ruleContext.aceCache.PT_NODE_HIDDEN);
-		Firebug.Console.log(ruleContext);
